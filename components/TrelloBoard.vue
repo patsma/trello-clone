e6c5116407849551616f1c1feae43f1dcc48df6e
Patryk Smakosz <pas@baernholdt.com>
2023-11-07 02:05:13 +0100

    feature(task): add & remove column

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class="flex items-start gap-4 overflow-x-auto">
     <draggable
         v-model="columns"
         group="columns"
         :animation="200"
         handle=".drag-handle"
         item-key="id"
-        class="flex items-start gap-8 overflow-x-auto p-4"
+        class="flex items-start gap-8 p-4"
     >
       <template #item="{ element: column }: { element: Column }">
         <div class="rounded bg-gray-200 p-4 column task min-w-[250px]">
@@ -14,9 +14,13 @@
               class="grid grid-flow-col items-center justify-start gap-2 p-4 font-bold task__header align-center"
           >
             <DragHandle/>
-            <div class="grid pt-1">{{ column.title }}</div>
+            <input
+                class="title-input w-4/5 rounded bg-transparent px-1 focus:bg-white mt-1"
+                v-model="column.title"
+                @keyup.enter="($event.target as HTMLInputElement).blur()"
+                @keydown.backspace="column.title ==='' ? (columns = columns.filter(c => c.id !== column.id)) : null"
+                type="text">
           </div>
-
           <draggable
               v-model="column.tasks"
               :group="{ name: 'tasks', pull: alt ? 'clone' : true}"
@@ -33,13 +37,18 @@
               </div>
             </template>
           </draggable>
-
-          <footer class=" p-2">
+          <footer class="p-2">
             <NewTask @add="column.tasks.push($event)"/>
           </footer>
         </div>
       </template>
     </draggable>
+    <button
+        @click="createColumn"
+        class="whitespace-nowrap rounded bg-gray-200 p-4 mt-4 opacity-50"
+    >
+      + Add Column
+    </button>
   </div>
 </template>
 <script lang="ts" setup>
@@ -92,4 +101,18 @@ const columns = ref<Column[]>([
   },
 ]);
 const alt = useKeyModifier("Alt")
+
+function createColumn() {
+  const column: Column = {
+    title: "",
+    id: nanoid(),
+    tasks: [],
+  };
+  columns.value.push(column);
+  nextTick(() => {
+
+    (document.querySelector('.column:last-of-type .title-input') as HTMLInputElement).focus();
+
+  })
+}
 </script>
